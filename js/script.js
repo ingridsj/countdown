9e37cc1994c98ae63253f81face3af8af235f2e7
Ingrid Almeida <ingridsjalmeida@gmail.com>
2021-01-08 11:06:18 -0300

    adding script

--- js/script.js
+++ js/script.js
@@ -2,6 +2,10 @@ let dateInput = document.querySelector(".dateInput")
 let startButton = document.querySelector(".startButton")
 
 startButton.addEventListener("click", () => {
-
+    if(!dateInput.value){
+        window.alert("Please enter a valid date.")
+    } else {
+        
+    }
 
 })
